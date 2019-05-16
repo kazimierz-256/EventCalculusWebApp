9644997923cc9549c81729823dc121bba0c4bd4e
Kazimierz Wojciechowski <wojciechowskik@student.mini.pw.edu.pl>
2019-05-16 23:15:31 +0200

    formula evaluaion

--- apps/concurrent/test/formulaTrue.pl
+++ apps/concurrent/test/formulaTrue.pl
@@ -4,3 +4,8 @@
 logic_formula_satisfied("LOADED", LIST),
 logic_formula_satisfied(and("LOADED", negate("DEAD")), LIST),
 logic_formula_satisfied(iff(negate("LOADED"), negate(negate(or("DEAD", "DEAD")))), LIST).
+
+
+:- list_to_assoc(["LOADED"-true, "DEAD"-Q], LIST),
+logic_formula_satisfied("DEAD", LIST),
+Q = true.
