8eaa9141ca83f4c4cd059f91a8c3a1509d8dd9a0
Kazimierz Wojciechowski <wojciechowskik@student.mini.pw.edu.pl>
2019-05-25 19:13:45 +0200

    beautiful next_state

--- apps/concurrent/modules/next_state.pl
+++ apps/concurrent/modules/next_state.pl
@@ -30,23 +30,20 @@ generate_new_assignment(Assignment, Fluent_Assignments, Occlusion_List, New_Assi
 get_next_state(Time, Fluent_Assignments, Observations, ACS, Action_Domain, New_Assignment) :-
 
 get_next_state(Time, Fluent_Assignments, Observations, ACS, Action_Domain, New_Assignment) :-
-    get_assoc(Time, ACS, ACS_Compound_Action) -> 
+    (get_assoc(Time, Observations, Observation) -> logic_formula_satisfied(Observation, Fluent_Assignments) ; true),
+    (get_assoc(Time, ACS, ACS_Compound_Action) -> 
     (
-        (get_assoc(Time, Observations, Observation) -> logic_formula_satisfied(Observation, Fluent_Assignments) ; true),
         % assoc_to_list(Action_Domain, Action_Domain_List),
         findall(Action, 
             (member(Action, ACS_Compound_Action), potentially_executable_atomic(Time, Action_Domain, Fluent_Assignments, Action))
             , Compound_Action),
         dif(Compound_Action, []),
-        % could have duplicates
         findall(Fluent, (member(Action, Compound_Action), get_occlusion(Action, Action_Domain, Fluent)), Occlusion_List),
         sort(Occlusion_List, Unique_Occlusion_List),
-        % get some MNS decomposition
         get_action_decomposition(Compound_Action, Time, Action_Domain, Fluent_Assignments, Sample_MNS_Decomposition),
         compound_executable_atomic_get_assignment(Time, Action_Domain, Fluent_Assignments, Sample_MNS_Decomposition, Assignment),
         % from assignment generate new state
         generate_new_assignment(Assignment, Fluent_Assignments, Unique_Occlusion_List, New_Assignment)
-    
     )
-    ; New_Assignment = Fluent_Assignments.
+    ; New_Assignment = Fluent_Assignments).
     
