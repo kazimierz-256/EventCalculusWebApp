400f9103b5a283c654fe611f601a5474e0195e8c
Aleksandra Bulka <aleksandra.bulka@altalog.pl>
2019-05-25 03:37:28 +0200

    Added 'scenario Sc' suffix for documentation compatibility

--- apps/concurrent/js/query.js
+++ apps/concurrent/js/query.js
@@ -16,37 +16,37 @@ class Query {
     getQueryFullText(){return this.full_text}
 }
 const query_collection = {
-    1: {id: 1, type_text: 'Necessarily executable'},
-    2: {id: 2, type_text: 'Possibly executable'},
-    3: {id: 3, type_text: 'Necessarily accessible γ at t'},
-    4: {id: 4, type_text: 'Possibly accessible γ at t'},
-    5: {id: 5, type_text: 'Necessarily executable A at t'},
-    6: {id: 6, type_text: 'Possibly executable A at t'},
+    1: {id: 1, type_text: 'Necessarily executable scenario Sc'},
+    2: {id: 2, type_text: 'Possibly executable scenario Sc'},
+    3: {id: 3, type_text: 'Necessarily accessible γ at t when Sc'},
+    4: {id: 4, type_text: 'Possibly accessible γ at t when Sc'},
+    5: {id: 5, type_text: 'Necessarily executable A at t when Sc'},
+    6: {id: 6, type_text: 'Possibly executable A at t when Sc'},
   }
 var QuerySectionEventHandlers = {
 shouldTimeBeHidden: function(query, prop_name){
-    if (!(query.type_text == 'Necessarily executable' || query.type_text == 'Possibly executable' || query.type_text == ``))
+    if (!(query.type_text == 'Necessarily executable scenario Sc' || query.type_text == 'Possibly executable scenario Sc' || query.type_text == ``))
         return true;
 },
 shouldActionBeHidden: function(query, prop_name){
-    if (query.type_text == 'Necessarily executable A at t' || query.type_text == 'Possibly executable A at t')
+    if (query.type_text == 'Necessarily executable A at t when Sc' || query.type_text == 'Possibly executable A at t when Sc')
         return true;
 },
 shouldCondBeHidden: function(query, prop_name){
-    if (query.type_text == 'Possibly accessible γ at t' || query.type_text == 'Necessarily accessible γ at t')
+    if (query.type_text == 'Possibly accessible γ at t when Sc' || query.type_text == 'Necessarily accessible γ at t when Sc')
         return true;
 },
 rebuildQueryText: function(query){
-    if (query.type_text == 'Necessarily executable' || query.type_text == 'Possibly executable')
+    if (query.type_text == 'Necessarily executable scenario Sc' || query.type_text == 'Possibly executable scenario Sc')
         query.full_text = query.type_text;
-    if (query.type_text == 'Necessarily accessible γ at t')
-        query.full_text = 'Necessarily accessible ' + query.condition + ' at ' + query.time;
-    if (query.type_text == 'Possibly accessible γ at t')
-        query.full_text = 'Possibly accessible ' + query.condition + ' at ' + query.time;
-    if (query.type_text == 'Necessarily executable A at t')
-        query.full_text = 'Necessarily executable ' + query.action + ' at ' + query.time;
-    if (query.type_text == 'Possibly executable A at t')
-        query.full_text = 'Possibly executable ' + query.action + ' at ' + query.time;
+    if (query.type_text == 'Necessarily accessible γ at t when Sc')
+        query.full_text = 'Necessarily accessible ' + query.condition + ' at ' + query.time + ' when Sc';
+    if (query.type_text == 'Possibly accessible γ at t when Sc')
+        query.full_text = 'Possibly accessible ' + query.condition + ' at ' + query.time + ' when Sc';
+    if (query.type_text == 'Necessarily executable A at t when Sc')
+        query.full_text = 'Necessarily executable ' + query.action + ' at ' + query.time + ' when Sc';
+    if (query.type_text == 'Possibly executable A at t when Sc')
+        query.full_text = 'Possibly executable ' + query.action + ' at ' + query.time + ' when Sc';
 },
 changedProperty(event, query){
     this.rebuildQueryText(query)
