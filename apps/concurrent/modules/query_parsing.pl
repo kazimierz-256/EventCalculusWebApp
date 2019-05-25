8dd240d317e38316dec39727ca356c0923cbfd5f
Kazimierz Wojciechowski <wojciechowskik@student.mini.pw.edu.pl>
2019-05-25 15:30:37 +0200

    query parsing update

--- apps/concurrent/modules/query_parsing.pl
+++ apps/concurrent/modules/query_parsing.pl
@@ -1,21 +1,21 @@
 :- module(query_parsing, 
     [
-        query_parsing/2
+        get_query_from_text/2
 	]).
 
 :- use_module(library(dcg/basics)).
-:- use_module(logic_tree_pasring).
+:- use_module(logic_tree_parsing).
 
 csv_to_list([S]) --> fluent(F),{string_chars(S, F)}.
 csv_to_list([S|Others]) --> fluent(F),{string_chars(S, F)},([',']|[',',' ']),csv_to_list(Others).
 
 
 parse_query(necessarily_executable) --> parse_string("necessarily executable").
-parse_query(possibly_executable) --> parse_string("possible executable").
+parse_query(possibly_executable) --> parse_string("possibly executable").
 parse_query(necessarily_accessible(Tree, Time)) -->
     parse_string("necessarily accessible "), logictree(Tree), parse_string(" at "), integer(Time).
 parse_query(possibly_accessible(Tree, Time)) -->
-    parse_string("possible accessible "), logictree(Tree), parse_string(" at "), integer(Time). 
+    parse_string("possibly accessible "), logictree(Tree), parse_string(" at "), integer(Time). 
 parse_query(necessarily_executable(Action, Time)) -->
     parse_string("necessarily executable "), csv_to_list(Action), parse_string(" at "), integer(Time).
 parse_query(possibly_executable(Action, Time)) -->
@@ -23,4 +23,11 @@ parse_query(possibly_executable(Action, Time)) -->
 
 get_query_from_text(Query, Text) :- 
     normalize_space(string(String_Cleaned), Text),
-    phrase(parse_query(Query), String_Cleaned).
+    string_chars(String_Cleaned, Chars),
+    phrase(parse_query(Query), Chars).
+
+:- 
+    % get_query_from_text(Query, "necessarily executable"),
+    % writeln(Query).
+    get_query_from_text(Query, "possibly accessible ALIVE1 and not ALIVE2 and not ALIVE3 at 5"),
+    write(Query).
