99ea12c1eb661194dbaae2a71d14194101f8b7f9
Kazimierz Wojciechowski <wojciechowskik@student.mini.pw.edu.pl>
2019-05-27 02:41:07 +0200

    prepared for implementation

--- apps/concurrent/modules/next_state.pl
+++ apps/concurrent/modules/next_state.pl
@@ -20,23 +20,22 @@ get_sample_fluent_assignment([F|Fluents], Assoc_Including_F) :-
     (F_Value = true ; F_Value = false),
     put_assoc(F, A, F_Value, Assoc_Including_F).
 
-% generate_new_assignment(Assignment, Fluent_Assignments, Occlusion_List, New_Assignment) :-
-%     % for each visible fluent
-%     % if it is in Assignment, there is no ambiguity
-%     % otherwise if it is in Occluesion List, there is ambiguity
-%     % otherwise keep Fluent_Assignments value
-%     findall(Fluent-Value, (
-%             get_assoc(Fluent, Assignment, Value) -> true
-%             ;
-%             (
-%                 member(Fluent, Occlusion_List) ->
-%                     (Value = true ; Value = false)
-%                     ; get_assoc(Fluent, Fluent_Assignments, Value)
-%             )
-%         ), Assignments),
-%     list_to_assoc(Assignments, New_Assignment).
 
 % THIS HERE IS TOTALLY NOT READY, shoud implement new assignment based on multiple conditions
+% generate all possible assignments
+% use the following predicate get_sample_fluent_assignment
+% should determine all fluents taking part in: 
+%   Fluent_Assignments
+%   next time observation if any
+%   occlusion list (already found)
+% must satisfy:
+%  the only variables that change in 2^n ways are those from Unique_Occlusion_List and those in upcoming observation that are new, they're absent in Fluent assignments and occlusion
+%  others must remain intact as in Fluent_Assignments
+%  the conjunction of all consequences of MNS_Executed_Action (extract Consequence from compound_executable_atomic_get_assignment?)
+%  Observation from next time must hold so if there are new fluents (they were smthing at the beginning but only now we know)
+get_valid_assignment(Unique_Occlusion_List, Fluent_Assignments, Next_Observation, New_Assignment).
+get_valid_assignment(Unique_Occlusion_List, Fluent_Assignments, New_Assignment).
+
 get_next_state(Time, Fluent_Assignments, Observations, Actions, Action_Domain, MNS_Executed_Action, New_Assignment) :-
     % (get_assoc(Time, Observations, Observation) -> logic_formula_satisfied(Observation, Fluent_Assignments) ; true),
     (get_assoc(Time, Actions, ACS_Compound_Action) -> 
@@ -50,25 +49,23 @@ get_next_state(Time, Fluent_Assignments, Observations, Actions, Action_Domain, M
         findall(Fluent, (member(Action, Compound_Action), get_occlusion(Action, Action_Domain, Fluent)), Occlusion_List),
         sort(Occlusion_List, Unique_Occlusion_List),
         
-        % generate all possible assignments
-        % use the following predicate get_sample_fluent_assignment
-        % should determine all fluents taking part in: 
-        %   Fluent_Assignments
-        %   next time observation if any
-        %   occlusion list (already found)
-        % must satisfy:
-        %  the only variables that change in 2^n ways are those from Unique_Occlusion_List and those in upcoming observation that are new, they're absent in Fluent assignments and occlusion
-        %  others must remain intact as in Fluent_Assignments
-        %  the conjunction of all consequences of MNS_Executed_Action (extract Consequence from compound_executable_atomic_get_assignment?)
-        %  Observation from next time must hold so if there are new fluents (they were smthing at the beginning but only now we know)
         Next_Time = Time + 1,
         (get_assoc(Next_Time, Observations, Next_Observation) ->
             get_valid_assignment(Unique_Occlusion_List, Fluent_Assignments, Next_Observation, New_Assignment).
         ;
-            empty_assoc(No_Observation),
-            get_valid_assignment(Unique_Occlusion_List, Fluent_Assignments, No_Observation, New_Assignment).
+            get_valid_assignment(Unique_Occlusion_List, Fluent_Assignments, New_Assignment).
         )
     )
     % WAIT, next condition must be satisfied!
-    ; New_Assignment = Fluent_Assignments).
+    ; 
+    (
+        Next_Time = Time + 1,
+        (get_assoc(Next_Time, Observations, Next_Observation) ->
+            logic_formula_satisfied(Next_Observation, Fluent_Assignments),
+            New_Assignment = Fluent_Assignments
+        ;
+            New_Assignment = Fluent_Assignments
+        )
+    )
+    ).
     
