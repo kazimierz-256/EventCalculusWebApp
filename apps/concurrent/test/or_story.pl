23a26a925a73d7d966e1f95e56e019bbdf886657
Aleksandra <al.byczynska@gmail.com>
2019-05-29 10:23:14 +0200

    fix or_story

--- apps/concurrent/test/or_story.pl
+++ apps/concurrent/test/or_story.pl
@@ -2,34 +2,48 @@
 :- use_module("../modules/query_parsing.pl").
 :- use_module("../concurrent.pl").
 
-:-  parse_domain("A causes f or not f", Domain),
+possibly_executable_a_causes_f_or_not_f :-
+    parse_domain("A causes f or not f", Domain),
     parse_acs("A|0", Acs),
     parse_obs("not f|0", Obs),
     Scenario = (Obs, Acs),
     get_query_from_text(Query, 'possibly executable'),
     run_scenario(Scenario, Domain, Query).
 
-:-  parse_domain("A causes f or not f", Domain),
+necessarily_executable_a_causes_f_or_not_f :-
+    parse_domain("A causes f or not f", Domain),
     parse_acs("A|0", Acs),
     parse_obs("not f|0", Obs),
     Scenario = (Obs, Acs),
     get_query_from_text(Query, 'necessarily executable'),
     run_scenario(Scenario, Domain, Query).
 
-% TODO this is not working -> A should be both true and false at 5 (2 models)
-:-  parse_domain("A causes f or not f", Domain),
-   parse_acs("A|0", Acs),
-   parse_obs("not f|0", Obs),
-   Scenario = (Obs, Acs),
-   get_query_from_text(Query, 'possibly executable A at 5'),
-   run_scenario(Scenario, Domain, Query),
-   get_query_from_text(Query, 'possibly accessible f at 5'),
-   run_scenario(Scenario, Domain, Query).
+possibly_executable_a_at_5 :-
+    parse_domain("A causes f or not f", Domain),
+    parse_acs("A|0", Acs),
+    parse_obs("not f|0", Obs),
+    Scenario = (Obs, Acs),
+    get_query_from_text(Query, 'possibly accessible f at 5'),
+    run_scenario(Scenario, Domain, Query).
 
-:-  parse_domain("A causes f or not f", Domain),
+possibly_executable_a_at_5_neg :-
+    parse_domain("A causes f or not f", Domain),
+    parse_acs("A|0", Acs),
+    parse_obs("not f|0", Obs),
+    Scenario = (Obs, Acs),
+    get_query_from_text(Query, 'possibly accessible not f at 5'),
+    run_scenario(Scenario, Domain, Query).
+
+necessarily_executable_a_at_5 :-
+    parse_domain("A causes f or not f", Domain),
     parse_acs("A|0", Acs),
     parse_obs("not f|0", Obs),
     Scenario = (Obs, Acs),
     get_query_from_text(Query, 'necessarily accessible A at 5'),
     not(run_scenario(Scenario, Domain, Query)).
 
+:- possibly_executable_a_causes_f_or_not_f.
+:- necessarily_executable_a_causes_f_or_not_f.
+:- possibly_executable_a_at_5.
+:- possibly_executable_a_at_5_neg.
+:- necessarily_executable_a_at_5.
