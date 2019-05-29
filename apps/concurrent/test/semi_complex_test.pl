e27f27573e556f1e791a18a0ba40d6fbabca23cb
Aleksandra <al.byczynska@gmail.com>
2019-05-29 16:19:33 +0200

    add simpler not passing version

--- apps/concurrent/test/semi_complex_test.pl
+++ apps/concurrent/test/semi_complex_test.pl
@@ -3,42 +3,59 @@
 :- use_module("../concurrent.pl").
 
 
-possibly_executable :-
-    parse_domain(
-        'KUP causes MAM or not MAM
-OGLADAJ1 causes HAPPY if MAM
-OGLADAJ2 causes not HAPPY if not MAM'
-UKRADNIJ causes MAM if not MAM,
-         Domain),
-    parse_acs(
-        'KUP|0
-OGLADAJ1, OGLADAJ2|1
-UKRADNIJ|2
-OGLADAJ1, OGLADAJ2|3',
-        Acs),
-    parse_obs('not MAM and not HAPPY|0', Obs),
-    Scenario = (Obs, Acs),
-    get_query_from_text(Query, 'possibly executable'),
-    run_scenario(Scenario, Domain, Query).
+%possibly_executable :-
+%    parse_domain(
+%        'KUP causes MAM or not MAM
+%OGLADAJ1 causes HAPPY if MAM
+%OGLADAJ2 causes not HAPPY if not MAM
+%UKRADNIJ causes MAM if not MAM',
+%         Domain),
+%    parse_acs(
+%        'KUP|0
+%OGLADAJ1, OGLADAJ2|1
+%UKRADNIJ|2
+%OGLADAJ1, OGLADAJ2|3',
+%        Acs),
+%    parse_obs('not MAM and not HAPPY|0', Obs),
+%    Scenario = (Obs, Acs),
+%    get_query_from_text(Query, 'possibly executable'),
+%    run_scenario(Scenario, Domain, Query).
+
+%possibly_executable_KUP_at_0 :-
+%    parse_domain(
+%        'KUP causes MAM or not MAM
+%OGLADAJ1 causes HAPPY if MAM
+%OGLADAJ2 causes not HAPPY if not MAM
+%UKRADNIJ causes MAM if not MAM',
+%         Domain),
+%    parse_acs(
+%        'KUP|0
+%OGLADAJ1, OGLADAJ2|1
+%UKRADNIJ|2
+%OGLADAJ1, OGLADAJ2|3',
+%        Acs),
+%    parse_obs('not MAM and not HAPPY|0', Obs),
+%    Scenario = (Obs, Acs),
+%    writeln(Domain),
+%    writeln(Scenario),
+%    get_query_from_text(Query, 'possibly executable KUP at 0'),
+%    run_scenario(Scenario, Domain, Query).
 
 possibly_executable_KUP_at_0 :-
     parse_domain(
         'KUP causes MAM or not MAM
-OGLADAJ1 causes HAPPY if MAM
-OGLADAJ2 causes not HAPPY if not MAM'
-UKRADNIJ causes MAM if not MAM,
+OGLADAJ2 causes not HAPPY if not MAM
+OGLADAJ1 causes HAPPY if MAM',
          Domain),
     parse_acs(
-        'KUP|0
-OGLADAJ1, OGLADAJ2|1
-UKRADNIJ|2
-OGLADAJ1, OGLADAJ2|3',
+        'KUP|0',
         Acs),
     parse_obs('not MAM and not HAPPY|0', Obs),
     Scenario = (Obs, Acs),
-    get_query_from_text(Query, 'possibly executable KUP at 0'),
+    writeln(Domain),
+    writeln(Scenario),
+    get_query_from_text(Query, 'necessarily executable KUP at 0'),
     run_scenario(Scenario, Domain, Query).
 
-
-:- possibly_executable.
+%:- possibly_executable.
 :- possibly_executable_KUP_at_0.
