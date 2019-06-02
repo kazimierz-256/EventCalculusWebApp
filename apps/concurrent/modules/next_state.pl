89b0e5594f5b8195e1b9867b808b60ee80cfb63d
Kazimierz Wojciechowski <wojciechowskik@student.mini.pw.edu.pl>
2019-06-02 16:17:06 +0200

    pretty formatting

--- apps/concurrent/modules/next_state.pl
+++ apps/concurrent/modules/next_state.pl
@@ -21,39 +21,46 @@ conjunct(Statement, Acc1, and(Statement, Acc1)).
 
 get_next_state(Time, Fluent_Assignments, Observations, Actions, Action_Domain, MNS_Executed_Action, New_Assignment) :-
     get_assoc(Time, Actions, ACS_Compound_Action)
-    ->
-    (
-        findall(
-            Action,
-            (member(Action, ACS_Compound_Action), potentially_executable_atomic(Time, Action_Domain, Fluent_Assignments, Action)),
-            Potentailly_Executable_Subset),
-        dif(Potentailly_Executable_Subset, []),
-        
-        get_nonempty_action_decomposition(Potentailly_Executable_Subset, Time, Action_Domain, Fluent_Assignments, MNS_Executed_Action),
-        findall(Fluent, get_total_occlusion(MNS_Executed_Action, Action_Domain, Fluent), Occlusion_List),
-        sort(Occlusion_List, Unique_Occlusion_List),
-        Next_Time is Time + 1,
-        %prepare causes postconditions
-        
-        findall(
-            Causes_Condition,
-            (member(Action, MNS_Executed_Action),get_assoc(Action, Action_Domain, Action_Description),  get_assoc("causes", Action_Description, (Causes_Condition, _))),
-            Causes_Conditions),
-        foldl(conjunct, Causes_Conditions, true, Consequence),
-        (get_assoc(Next_Time, Observations, Next_Observation)
-        ->  get_association_satisfying_formula(Unique_Occlusion_List, Fluent_Assignments, and(Consequence, Next_Observation), New_Assignment)
-        ;   get_association_satisfying_formula(Unique_Occlusion_List, Fluent_Assignments, Consequence, New_Assignment)
+    ->  (
+            findall(
+                Action,
+                (
+                    member(Action, ACS_Compound_Action),
+                    potentially_executable_atomic(Time, Action_Domain, Fluent_Assignments, Action)
+                ),
+                Potentailly_Executable_Subset),
+            dif(Potentailly_Executable_Subset, []),
+            get_nonempty_action_decomposition(Potentailly_Executable_Subset, Time, Action_Domain, Fluent_Assignments, MNS_Executed_Action),
+            findall(
+                Fluent,
+                get_total_occlusion(MNS_Executed_Action, Action_Domain, Fluent),
+                Occlusion_List
+            ),
+            sort(Occlusion_List, Unique_Occlusion_List),
+            Next_Time is Time + 1,
+            findall(
+                Causes_Condition,
+                (
+                    member(Action, MNS_Executed_Action),
+                    get_assoc(Action, Action_Domain, Action_Description),
+                    get_assoc("causes", Action_Description, (Causes_Condition, _))
+                ),
+                Causes_Conditions
+            ),
+            foldl(conjunct, Causes_Conditions, true, Consequence),
+            (
+                get_assoc(Next_Time, Observations, Next_Observation)
+                ->  get_association_satisfying_formula(Unique_Occlusion_List, Fluent_Assignments, and(Consequence, Next_Observation), New_Assignment)
+                ;   get_association_satisfying_formula(Unique_Occlusion_List, Fluent_Assignments, Consequence, New_Assignment)
+            )
         )
-    )
-    ;
-    (
-        MNS_Executed_Action = [],
-        Next_Time is Time + 1,
-        (get_assoc(Next_Time, Observations, Next_Observation)
-        ->
-            logic_formula_satisfied(Next_Observation, Fluent_Assignments),
-            New_Assignment = Fluent_Assignments
-        ;
-            New_Assignment = Fluent_Assignments
-        )
-    ).
+    ;   (
+            MNS_Executed_Action = [],
+            Next_Time is Time + 1,
+            (
+                get_assoc(Next_Time, Observations, Next_Observation)
+                ->  logic_formula_satisfied(Next_Observation, Fluent_Assignments),
+                    New_Assignment = Fluent_Assignments
+                ;   New_Assignment = Fluent_Assignments
+            )
+        ).
